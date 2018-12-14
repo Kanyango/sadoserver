6cbdcb8036449e4296cb14ee87b45e849b13cfbb
Kanyango <kariukikanyango@gmail.com>
2018-12-14 20:01:57 +0300

    Update payments.js

--- services/payments.js
+++ services/payments.js
@@ -26,7 +26,7 @@ var payments = {
         
 	request(
 		{
-        url   : 'https://sandbox.api.visa.com/cybersource/v2/payments?apikey={9XS2NUQR97HTKTJQGOWI21K--tSViYxPKgmJ8oPbbtacEqv0k}',
+        url   : 'https://sandbox.api.visa.com/cybersource/v2/payments?apikey=9XS2NUQR97HTKTJQGOWI21K--tSViYxPKgmJ8oPbbtacEqv0k',
 	method: 'POST',
 	headers:  {name: 'content-type', value: 'application/json',
 		  name: 'x-pay-token', value: xPayToken },
