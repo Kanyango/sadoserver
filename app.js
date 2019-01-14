4ee04ec2c073876bf12f19d8411b35b8c049edfa
Kanyango <kariukikanyango@gmail.com>
2019-01-14 13:44:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");  
     //res.header('Access-Control-Expose-Headers', 'Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'content-type , Authorization','x-pay-token');
+    res.header('Access-Control-Allow-Headers', 'content-type', 'Authorization','x-pay-token');
     next();
 });
 
